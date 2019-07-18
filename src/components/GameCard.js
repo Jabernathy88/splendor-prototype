abcb01239eb5bd9063eedd69b046d9955451bc30
Jeremy Abernathy <j.e.abernathy@gmail.com>
2019-07-18 11:48:20 -0400

    Temporary layout.

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class GameCard extends Component {
   render() {
     return (
-      <div className="h-75 w-25 border border-dark rounded-lg">
+      <div className="h-75 w-25 border border-white rounded-lg bg-white">
         <p>Hello from GameCard</p>
         
       </div>
