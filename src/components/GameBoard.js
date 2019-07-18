3aff01c5f09137f7b8bf9d7d947d0c22f2c9a836
Jeremy Abernathy <j.e.abernathy@gmail.com>
2019-07-18 11:27:03 -0400

    Initial commit. Basic components. Clear boilerplate html.

---
+++ src/components/GameBoard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import GameCard from './GameCard';
+
+const GameBoard = () => {
+  return (
+    <div>
+      Hello from GameBoard
+      <GameCard />
+      
+    </div>
+  );
+};
+
+export default GameBoard;
