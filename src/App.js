e3f6e3e2a3f2d67452d0f1217b879972e2375a64
Jeremy Abernathy <j.e.abernathy@gmail.com>
2019-07-18 14:10:50 -0400

    Test deployment. Test viewport re iPad.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ function App() {
   return (
     <div id="content">
       <AppHeader />
+      <br />
+      <div className="ipad-test border border-dark">
+        I am a div: 1020 x 762
+      </div>
       <GameBoard />
 
     </div>
