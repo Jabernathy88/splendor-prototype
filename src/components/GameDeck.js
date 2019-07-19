6846eb5f6fe44f71c0d9b22f1f713337cb06bb47
Jeremy Abernathy <j.e.abernathy@gmail.com>
2019-07-19 13:46:35 -0400

    Add components.

---
+++ src/components/GameDeck.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const GameDeck = () => {
+  return (
+    <div>
+      Hello from GameDeck
+    </div>
+  );
+};
+
+export default GameDeck;
---
+++ src/components/NobleCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NobleCard = () => {
+  return (
+    <div>
+      Hello from NobleCard
+    </div>
+  );
+};
+
+export default NobleCard;
